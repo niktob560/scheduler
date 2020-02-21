e7d0dd37133600f60d608bb43c56d15411ade783
niktob560 <niktob560@gmail.com>
2020-02-21 09:25:34 +0300

    cleaned build

---
+++ Build/CMakeFiles/disassemble_scheduler.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disassemble_scheduler"
+  "scheduler-atmega2560.map"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/disassemble_scheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/CMakeFiles/upload_scheduler_eeprom.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/upload_scheduler_eeprom"
+  "scheduler-atmega2560-eeprom.hex"
+  "scheduler-atmega2560.map"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/upload_scheduler_eeprom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
